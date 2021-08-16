affd3968be20f51a9aec3fdc869d3cc557bf9231
magnae2016 <magnae2016@gmail.com>
2021-08-16 21:44:33 +0900

    Add Redirect to /Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/Home'
 import Equipment from './components/pages/Equipment'
@@ -11,6 +16,7 @@ function App() {
                     <Router>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/home" component={Home} />
+                        <Redirect to="/home" />
                         <Switch>
                             <Route path="/equipment" component={Equipment} />
                         </Switch>
