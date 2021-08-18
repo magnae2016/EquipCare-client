9b036b0889fb0ac2170081591e51cb08c1f0ee8d
magnae2016 <magnae2016@gmail.com>
2021-08-18 20:18:47 +0900

    Add List to EquipmentByAlarm

--- src/components/UI/organisms/EquipmentByAlarm/index.jsx
+++ src/components/UI/organisms/EquipmentByAlarm/index.jsx
@@ -1,21 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import moment from 'moment'
 import './index.css'
 import ApiSubjectBx from '@/components/UI/molecules/apiSubjectBx'
-import { Link } from 'react-router-dom'
 
-function ListEmpty(params) {
+function ListEmpty({ warning, description }) {
     return (
         <div className="sggt_container">
             <div className="nx_suggest_recent_nothing">
-                최근 검색어 내역이 없습니다.
+                {warning}
                 <br />
-                <span className="kwd_dsc">설정이 초기화된다면</span>
+                <span className="kwd_dsc">{description}</span>
             </div>
         </div>
     )
 }
 
+function List({ datas, isFirst }) {
+    const { EQ_NAME, EQ_TESTER, COUNT, ratio } = datas
+    return (
+        <li className={`status_item party100 ${isFirst ? 'is_first' : ''}`}>
+            <div className="district_candidate">
+                <span className="candidate_name">{EQ_NAME}</span>
+                <em className="candidate_party">{EQ_TESTER}</em>
+            </div>
+            <div className="district_rate">
+                <span className="rate_txt">
+                    {ratio.toFixed(1)}
+                    <span className="rate_per">%</span>
+                </span>
+            </div>
+            <div className="district_graph">
+                <div className="graph_bar">
+                    <span
+                        className="graph_bg"
+                        style={{ left: `${ratio.toFixed(1)}%` }}
+                    ></span>
+                </div>
+            </div>
+            <div className="district_count">
+                <span className="count_txt">{COUNT}</span>
+            </div>
+        </li>
+    )
+}
+
 function EquipmentByAlarm({ keyword }) {
+    const [count, setCount] = useState(undefined)
+    const [orderedList, setOrderedList] = useState([])
+    const [updatedAt, setUpdatedAt] = useState(undefined)
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get(
+                `/api/alarms/${keyword}/equipments`
+            )
+            console.log(response.data)
+            const count = response.data.count
+            const datas = response.data.datas
+            let current = datas.map((element) => ({
+                ...element,
+                ratio: (element.COUNT / count) * 100,
+            }))
+            console.log(current)
+            setOrderedList(current)
+            setCount(count)
+            setUpdatedAt(response.data.last_update_time)
+        }
+        fetchData()
+    }, [])
+
     return (
         <section id="list-item-4">
             <ApiSubjectBx title="알람별 장비 현황">
@@ -39,7 +94,40 @@ function EquipmentByAlarm({ keyword }) {
                         </div>
                     </div>
                     <div className="district_scroll">
-                        <ListEmpty />
+                        {!keyword ? (
+                            <ListEmpty
+                                warning="알람 종류를 선택하지 않았습니다."
+                                description="알람 종류를 선택해주세요."
+                            />
+                        ) : count === 0 ? (
+                            <ListEmpty
+                                warning="당일 발생한 알람이 없습니다."
+                                description="다른 알람을 선택해주세요."
+                            />
+                        ) : (
+                            <div className="district_content">
+                                <div className="title">
+                                    <h3 className="title_txt">{keyword}</h3>
+                                    <em className="title_info title_info-chk">
+                                        {moment(updatedAt).format('HH:mm:ss')}{' '}
+                                        업데이트
+                                    </em>
+                                </div>
+                                <div className="status">
+                                    <ul className="status_list">
+                                        {orderedList.map((element, index) => (
+                                            <List
+                                                key={index}
+                                                datas={element}
+                                                isFirst={
+                                                    index === 0 ? true : false
+                                                }
+                                            />
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </ApiSubjectBx>
